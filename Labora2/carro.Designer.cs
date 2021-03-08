bd43e105095fd73bbd2ba6758128b8166fab8140
Abigail Gaitan <abi49386307@outlook.com>
2021-03-07 22:49:10 -0600

    Vehiculos

--- Labora2/carro.Designer.cs
+++ Labora2/carro.Designer.cs
@@ -29,12 +29,168 @@ namespace Labora2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Azul",
+            "Blanco",
+            "Celeste",
+            "Gris",
+            "Rojo",
+            "Verde"});
+            this.comboBox1.Location = new System.Drawing.Point(233, 183);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(154, 24);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(233, 227);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(154, 22);
+            this.textBox5.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 17);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Precio por kilometro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Color";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 17);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Marca";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Placa";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(233, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 22);
+            this.textBox3.TabIndex = 23;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 22);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 46);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Regresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 366);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 46);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // carro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "carro";
             this.Text = "carro";
+            this.Load += new System.EventHandler(this.carro_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Labora2/carro.cs
+++ Labora2/carro.cs
@@ -7,14 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
 namespace Labora2
 {
     public partial class carro : Form
     {
+        List<vehiculos> vehiculos = new List<vehiculos>();
+        string archivoV = "Registrovehículos.txt";
         public carro()
         {
             InitializeComponent();
+        }
+        public void guardar()
+        {
+            FileStream stream = new FileStream(archivoV, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+
+            for (int i = 0; i < vehiculos.Count; i++)
+            {
+                writer.WriteLine(vehiculos[i].Placa);
+                writer.WriteLine(vehiculos[i].Marca);
+                writer.WriteLine(vehiculos[i].Modelo);
+                writer.WriteLine(vehiculos[i].Color);
+                writer.WriteLine(vehiculos[i].Preciok);
+            }
+            writer.Close();
+        }
+        void leer_datos()
+        {
+            FileStream stream = new FileStream(archivoV, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                vehiculos tempvehiculos = new vehiculos();
+                tempvehiculos.Placa = reader.ReadLine();
+                tempvehiculos.Marca = reader.ReadLine();
+                tempvehiculos.Modelo = reader.ReadLine();
+                tempvehiculos.Color = reader.ReadLine();
+                tempvehiculos.Preciok = float.Parse(reader.ReadLine());
+                vehiculos.Add(tempvehiculos);
+
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
+        }
+        void limpiar()
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            comboBox1.Text = null;
+            textBox5.Text = null;
+        }
+
+        private int verificar_repeticion(string placa) 
+        {
+            int resultado = 0;
+            for (int i = 0; i < vehiculos.Count; i++)
+            {
+                if (vehiculos[i].Placa.Contains(placa))
+                    resultado = 1;
+            }
+            return resultado;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 vmenu = new Form1();
+            vmenu.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vehiculos tempvehiculo = new vehiculos();
+            tempvehiculo.Placa = textBox1.Text;
+            tempvehiculo.Marca = textBox2.Text;
+            tempvehiculo.Modelo = textBox3.Text;
+            tempvehiculo.Color = comboBox1.Text;
+            tempvehiculo.Preciok = float.Parse(textBox5.Text);
+
+            if (verificar_repeticion(textBox1.Text) != 1)
+            {
+                vehiculos.Add(tempvehiculo);
+                guardar();
+                limpiar();
+                MessageBox.Show("Vehiculo agregado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("No se pueden agregar vehiculos repetidos en la base de datos");
+            }
+        }
+
+        private void carro_Load(object sender, EventArgs e)
+        {
+            leer_datos();
         }
     }
 }
