25c96926e3867468c7375fd2492cf203699c2e39
Abigail Gaitan <abi49386307@outlook.com>
2021-03-07 22:31:47 -0600

    Formularios

--- Labora2/Form1.Designer.cs
+++ Labora2/Form1.Designer.cs
@@ -42,15 +42,17 @@ namespace Labora2
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar Clientes ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(60, 127);
+            this.button2.Location = new System.Drawing.Point(60, 137);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(147, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Agregar Vehiculos";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -60,6 +62,7 @@ namespace Labora2
             this.button3.TabIndex = 2;
             this.button3.Text = "Agregar Vehiculos";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
--- Labora2/Form1.cs
+++ Labora2/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Labora2
 {
     public partial class Form1 : Form
@@ -15,6 +16,27 @@ namespace Labora2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cliente vclientes = new cliente();
+            vclientes.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            carro carr = new carro();
+            carr.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Alquilado vclientes = new Alquilado();
+            vclientes.Show();
+            this.SetVisibleCore(false);
         }
     }
 }
